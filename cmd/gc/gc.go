7937eb8ef6e00ef00a404362a674380b48b406c6
Ryan Ogden <12yanogden@gmail.com>
2024-01-08 21:29:48 -0500

    add pterm

--- cmd/gc/gc.go
+++ cmd/gc/gc.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
 
-	"github.com/12yanogden/cli"
 	"github.com/12yanogden/git/internal/git"
 	"github.com/12yanogden/git/internal/ticket"
 	"github.com/12yanogden/shell"
+	"github.com/pterm/pterm"
 )
 
 func main() {
@@ -17,24 +16,51 @@ func main() {
 	ticket := ticket.BranchToTicket(branch)
 
 	if len(args) == 0 {
-		fmt.Println("gc: commit message required")
+		pterm.Error.Println("gc: commit message required")
 		os.Exit(1)
 	} else if len(args) > 1 {
-		fmt.Printf("gc: expected 1 argument. %d arguments given", len(args))
+		pterm.Error.Printf("gc: expected 1 argument. %d arguments given", len(args))
+		os.Exit(1)
 	}
 
-	if strings.Contains(branch, "develop") ||
-		strings.Contains(branch, "master") ||
+	if strings.Contains(branch, "master") ||
+		strings.Contains(branch, "main") ||
+		strings.Contains(branch, "dev") ||
 		strings.Contains(branch, "epic/") {
-		msg := "WARNING:\nBranch " + branch + " might be sensitive. Are you sure you want to commit? (y/n)"
 
-		if !cli.Confirm(msg) {
-			fmt.Println("No changes were added, committed, or pushed")
+		pterm.Warning.Printf("\nYou are committing directly to %s ", branch)
+
+		pterm.Println()
+		pterm.Println()
+
+		isConfirmed, _ := pterm.DefaultInteractiveConfirm.Show()
+
+		pterm.Println()
+
+		if !isConfirmed {
+			pterm.Info.Println("\nNo changes were added, committed, or pushed\n ")
+			pterm.Println()
 			os.Exit(1)
 		}
 	}
 
+	multi := pterm.DefaultMultiPrinter
+
+	addSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Add changes")
+	commitSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Add changes")
+	pushSpinner, _ := pterm.DefaultSpinner.WithWriter(multi.NewWriter()).Start("Add changes")
+
+	multi.Start()
+
 	shell.Run("git", []string{"add", "."})
+
+	addSpinner.Success()
+
 	shell.Run("git", []string{"commit", "-m", ticket + args[0]})
+
+	commitSpinner.Success()
+
 	shell.Run("git", []string{"push"})
+
+	pushSpinner.Success()
 }
--- go.mod
+++ go.mod
@@ -8,10 +8,21 @@ require (
 )
 
 require (
+	atomicgo.dev/cursor v0.2.0 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	atomicgo.dev/schedule v0.1.0 // indirect
 	github.com/12yanogden/cli v0.0.0-20231230154302-0730cf96ccbf // indirect
 	github.com/12yanogden/slices v0.0.0-20231230114623-01d02738a367 // indirect
 	github.com/12yanogden/str v0.0.0-20231230131640-bb6ec3aaf3b2 // indirect
 	github.com/12yanogden/strslices v0.0.0-20231229185917-5c654540c797 // indirect
+	github.com/containerd/console v1.0.3 // indirect
+	github.com/gookit/color v1.5.4 // indirect
+	github.com/lithammer/fuzzysearch v1.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pterm/pterm v0.12.74 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
